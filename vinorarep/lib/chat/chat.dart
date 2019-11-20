e1250be7c9ce5799b3bfe78f6dd272cf0150a717
chamodlakmal <chamodlakmal97@gmail.com>
2019-11-21 00:10:32 +0530

    chat updated

--- vinorarep/lib/chat/chat.dart
+++ vinorarep/lib/chat/chat.dart
@@ -1,183 +1,134 @@
 import 'dart:async';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'dart:io';
 import 'package:dash_chat/dash_chat.dart';
-  FirebaseUser user;
-  FirebaseAuth auth;
+import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';  
 class Chat extends StatefulWidget {
-  static const String id = "CHAT";
+  
   
   @override
   _ChatState createState() => _ChatState();
 }
 
 class _ChatState extends State<Chat> {
+  ChatUser user = ChatUser();
+  
   final FirebaseMessaging _fcm = FirebaseMessaging();
   StreamSubscription iosSubscription;
+  void getUserId() async{
+      final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+        FirebaseUser user1 = await _firebaseAuth.currentUser();
+        setState(() {
+          user.uid=user1.uid;
+          user.name=user1.email;
+        });
+       }
   @override
   void initState() {
-    
-    
+    user.name = "Name Loading ..";
+    user.uid = Uuid().v4().toString();
+    getUserId();
     super.initState();
-    if (Platform.isIOS) {
-            iosSubscription = _fcm.onIosSettingsRegistered.listen((data) {
-                // save the token  OR subscribe to a topic here
-            });
+    }
+    
+    void onSend(ChatMessage message) {
+    var documentReference = Firestore.instance
+        .collection('message')
+        .document(DateTime.now().millisecondsSinceEpoch.toString());
 
-            _fcm.requestNotificationPermissions(IosNotificationSettings());
-        }
-    auth = FirebaseAuth.instance;
-  getCurrentUser();
-    }
-    final Firestore _firestore = Firestore.instance;
-  
-    TextEditingController messageController = TextEditingController();
-    ScrollController scrollController = ScrollController(keepScrollOffset: true,initialScrollOffset: 5000000000);
-  
-    Future<void> callback() async {
-      if (messageController.text.length > 0) {
-        await _firestore.collection('messages').add({
-          'text': messageController.text,
-          'from': user.email,
-          'date': DateTime.now().toIso8601String().toString(),
-        });
-        messageController.clear();
-        scrollController.animateTo(
-          scrollController.position.maxScrollExtent,
-          curve: Curves.easeOut,
-          duration: const Duration(milliseconds: 300),
+    Firestore.instance.runTransaction((transaction) async {
+      await transaction.set(
+        documentReference,
+        message.toJson(),
+      );
+    });
+  }
+  void uploadFile() async {
+    File result = await ImagePicker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 100,
+      maxHeight: 400,
+      maxWidth: 400,
+    );
+
+    if (result != null) {
+      String id = Uuid().v4().toString();
+
+      final StorageReference storageRef =
+          FirebaseStorage.instance.ref().child("chat_images/$id.jpg");
+
+      StorageUploadTask uploadTask = storageRef.putFile(
+        result,
+        StorageMetadata(
+          contentType: 'image/jpg',
+        ),
+      );
+      StorageTaskSnapshot download = await uploadTask.onComplete;
+
+      String url = await download.ref.getDownloadURL();
+
+      ChatMessage message = ChatMessage(text: "", user: user, image: url);
+
+      var documentReference = Firestore.instance
+          .collection('message')
+          .document(DateTime.now().millisecondsSinceEpoch.toString());
+
+      Firestore.instance.runTransaction((transaction) async {
+        await transaction.set(
+          documentReference,
+          message.toJson(),
         );
-      }
+      });
     }
+  }
   
     @override
     Widget build(BuildContext context) {
       return Scaffold(
         appBar: AppBar(
-          title: Text("Royal Vintage Chat") ,
+          title: Text("Chat") ,
         ),
-        body: SafeArea(
-
-          child: Column(
-
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Expanded(
-
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: _firestore
-                      .collection('messages')
-                      .orderBy('date')
-                      .snapshots(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData)
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-  
-                    List<DocumentSnapshot> docs = snapshot.data.documents;
-  
-                    List<Widget> messages = docs
-                        .map((doc) => Message(
-                              from: doc.data['from'],
-                              text: doc.data['text'],
-                              me: user.email == doc.data['from'],
-                            ))
-                        .toList();
-  
-                    return ListView(
-                      padding: EdgeInsets.all(10.0),
-                      controller: scrollController,
-                      children: <Widget>[
-                        ...messages,
-                      ],
-                    );
-                  },
-                ),
+        body:  StreamBuilder(
+        stream: Firestore.instance.collection('message').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                    Theme.of(context).primaryColor),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        onSubmitted: (value) => callback(),
-                        decoration: InputDecoration(
-                          hintText: "Enter a Message...",
-                          border: const OutlineInputBorder(),
-                        ),
-                        controller: messageController,
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: (){
-                        callback();
-                        messageController.clear();
-                      },
-                      icon: Icon(Icons.send),
-                    )
-                  ],
-                ),
+            );
+          } else {
+            List<DocumentSnapshot> items = snapshot.data.documents;
+            var messages =
+                items.map((i) => ChatMessage.fromJson(i.data)).toList();
+            return DashChat(
+              user: user,
+              messages: messages,
+              inputDecoration: InputDecoration(
+                hintText: "Message here...",
+                border: InputBorder.none,
               ),
-            ],
-          ),
-        ),
+              
+              onSend: onSend,
+              scrollToBottom: false,
+              trailing: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.photo),
+                  onPressed: uploadFile,
+                )
+              ],
+            );
+          }
+        },
+      ),
       );
     }
   
-    void getCurrentUser() async {
-     user = await auth.currentUser();
-    }
-}
-class SendButton extends StatelessWidget {
-  final String text;
-  final VoidCallback callback;
-
-  const SendButton({Key key, this.text, this.callback}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return FlatButton(
-      color: Colors.orange,
-      onPressed: callback,
-      child: Text(text),
-    );
-  }
-}
-class Message extends StatelessWidget {
-  final String from;
-  final String text;
-
-  final bool me;
-
-  const Message({Key key, this.from, this.text, this.me}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        crossAxisAlignment:
-            me ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            from,
-          ),
-          Material(
-            color: me ? Colors.lightBlue : Colors.white,
-            borderRadius: BorderRadius.circular(4.0),
-            
-            child: Container(
-              padding: EdgeInsets.all(12.0),
-              child: Text(
-                text,
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
+    
 }
